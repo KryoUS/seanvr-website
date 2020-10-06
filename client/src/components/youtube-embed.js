580771c6bb315d7354afbafe71cc779217911374
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-05 23:27:23 -0600

    Added Origin to Youtube embed.

--- client/src/components/youtube-embed.js
+++ client/src/components/youtube-embed.js
@@ -4,7 +4,7 @@ class YoutubeEmbed extends React.Component {
     render() {
         return (
             <iframe title={`LatestYoutube${this.props.index}`} id="youtube" className="video-frame"
-                src={`https://www.youtube.com/embed/videoseries?list=UUTXez3yD0FbdgCuYwBlbrnQ&index=${this.props.index}&modestbranding=1`} 
+                src={`https://www.youtube.com/embed/videoseries?list=UUTXez3yD0FbdgCuYwBlbrnQ&index=${this.props.index}&modestbranding=1&origin=https://seanvr.net`} 
             />
         )
     }
