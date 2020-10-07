0f080fd1bff60a1fc4ea41e550a68af6c4f7c2f6
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-07 17:54:44 -0600

    Added dotenv and set dev. Layed groundwork for basic express server.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.DEV === true) {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+} else {
+    app.use(express.static(path.join(__dirname, '../var/www/seanvr.net/html')));
+}
 
 app.get("/", (req, res) => {
-    return res.sendFile(path.join(__dirname + "/build/index.html"));
+
+    if (process.env.DEV === true) {
+        return res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    } else {
+        return res.sendFile(path.join(__dirname + "../var/www/seanvr.net/html"));
+    }
+    
 });
 
 const port = process.env.PORT || 5000;
