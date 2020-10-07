5cbc009b0552de349371714e066d679ace8a62b0
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-07 16:53:48 -0600

    Moved Server to its own folder.

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/", (req, res) => {
-    return res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    return res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
