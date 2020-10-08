0d52a85e64cc92175f4178450fc9c15cf94a217b
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-07 19:26:04 -0600

    Attempting to fix Server pathing for mismatched routes.

--- server/index.js
+++ server/index.js
@@ -5,21 +5,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/build')));
-console.log(process.env.DEV);
+
 app.get("/", (req, res) => {
-    if (process.env.DEV === 'true') {
-        return res.sendFile(path.join(__dirname + "/../client/build/index.html"));
-    } else {
-        return res.sendFile(path.join(__dirname + "/client/build/index.html"));
-    }
+    return res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
 
 app.all("*", (req, res) => {
-    if (process.env.DEV === 'true') {
-        return res.sendFile(path.join(__dirname + "/../client/build/index.html"));
-    } else {
-        return res.sendFile(path.join(__dirname + "/client/build/index.html"));
-    }
+    return res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
