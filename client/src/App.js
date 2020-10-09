c00002161d7ba3b9d436f5473e94d6381e181064
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-09 03:44:00 -0600

    Created Warning component for underaged users. Moved App to new Content component. Added Content component to Warning component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,12 @@
 import React from 'react';
-import Twitch from './components/twitch-embed';
-import Youtube from './components/youtube-embed';
-import SocialLink from './components/socials-link';
+import Warning from './components/warning';
 import './App.css';
 import './Nav.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://d3mb8zw3wuinah.cloudfront.net/seanVRLogo.png" className="App-logo" alt="logo" />
-        <nav role="navigation">
-          <div id="menuToggle">
-            <input type="checkbox" />
-            <span></span>
-            <span></span>
-            <span></span>
-            <ul id="menu">
-              <SocialLink link="https://discord.gg/seanvr" image="discord_full.png" alt="discord" />
-              <SocialLink link="https://www.instagram.com/seanvrvr/" image="instagram_full.png" alt="instagram" />
-              <SocialLink link="https://www.tiktok.com/@sean_vr1" image="tiktok_full.png" alt="tiktok" />
-              <SocialLink link="https://www.twitch.tv/sean_vr" image="twitch_full.png" alt="twitchtv" />
-              <SocialLink link="https://twitter.com/SeanVr1/" image="twitter_full.png" alt="twitter" />
-              <SocialLink link="https://www.youtube.com/channel/UCTXez3yD0FbdgCuYwBlbrnQ" image="youtube_full.png" alt="youtube" />
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <div id="socials-large" className="flex-row-evenly">
-        <SocialLink link="https://discord.gg/seanvr" image="discord_full.png" alt="discord" />
-        <SocialLink link="https://www.instagram.com/seanvrvr/" image="instagram_full.png" alt="instagram" />
-        <SocialLink link="https://www.tiktok.com/@sean_vr1" image="tiktok_full.png" alt="tiktok" />
-        <SocialLink link="https://www.twitch.tv/sean_vr" image="twitch_full.png" alt="twitchtv" />
-        <SocialLink link="https://twitter.com/SeanVr1/" image="twitter_full.png" alt="twitter" />
-        <SocialLink link="https://www.youtube.com/channel/UCTXez3yD0FbdgCuYwBlbrnQ" image="youtube_full.png" alt="youtube" />
-      </div>
-      <div id="socials-mini" className="flex-row-evenly">
-        <SocialLink link="https://discord.gg/seanvr" image="discord_logo.png" alt="discord" />
-        <SocialLink link="https://www.instagram.com/seanvrvr/" image="instagram_logo.png" alt="instagram" />
-        <SocialLink link="https://www.tiktok.com/@sean_vr1" image="tiktok_logo.png" alt="tiktok" />
-        <SocialLink link="https://www.twitch.tv/sean_vr" image="twitch_logo.png" alt="twitchtv" />
-        <SocialLink link="https://twitter.com/SeanVr1/" image="twitter_logo.png" alt="twitter" />
-        <SocialLink link="https://www.youtube.com/channel/UCTXez3yD0FbdgCuYwBlbrnQ" image="youtube_logo.png" alt="youtube" />
-      </div>
-      <div className="flex-row-evenly">
-        <div id="col1" className="two-row-col">
-          <Youtube index={0} />
-          <Youtube index={1} />
-        </div>
-        <Twitch channel={'sean_vr'} />
-        <div id="col3" className="two-row-col">
-          <Youtube index={2} />
-          <Youtube index={3} />
-        </div>
-      </div>
-      <div className="gradient-line-black" />
-      <div className="flex-row-evenly">
-        <div id="about-header">About Me</div>
-        <img id="about-image" src="https://d3mb8zw3wuinah.cloudfront.net/seanVRAboutMe.png" alt="about SeanVR" />
-        <div id="about-text-container">
-          <p id="about-text">
-            Hello! Im Sean, the small sassy Scottish Fox who goes on crazy adventures, always says the wrong thing, and gets in trouble with his friends.
-            Im a full time streamer on Twitch.tv, a content creator on Youtube and TikTok, and am part of a wonderful community ran by my friends on Discord.
-            <br/><br/>
-            Come, join me as I adventure through VRChat with my friends as we explore crazy worlds and push the limits on Terms of Service.
-          </p>
-        </div>
-      </div>
-      <div className="gradient-line-black" />
-      <div className="socials-embeds">
-        <a className="twitter-timeline" data-height="600" data-width="400" data-theme="dark" href="https://twitter.com/SeanVr1?ref_src=twsrc%5Etfw">Tweets by SeanVr</a>
-        <iframe title="discord-embed" src="https://discordapp.com/widget?id=540600422185041936&theme=dark" width="400" height="600" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-      </div>
-      <div className="flex-row-evenly">
-        <div className="credits">Created with love by <a href="https://taurussho89.wixsite.com/portfolio">KryoUS</a> | 
-        Artwork by <a href="https://lilyth.art/">Lilyth.Art</a> | Coloring by <a href="https://tanksi.art/">Tanksi</a></div>
-      </div>
+      <Warning />
     </div>
   );
 }
---
+++ client/src/components/warning.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Content from './content';
+
+class Warning extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+        show: true
+    };
+  };
+
+  toggleWarning = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  };
+
+  render() {
+    return (
+      this.state.show ? <div className="warning-modal">
+        <img id="about-image" src="https://d3mb8zw3wuinah.cloudfront.net/seanVRAboutMe.png" alt="about SeanVR" />
+        <div className="warning-title">WARNING: This website may contain mature content!</div>
+        <div id="warning-subtext" className="warning-title">(By clicking "I Understand" you signify that you are at least 18 years of age.)</div>
+        <div className="flex-row-evenly">
+          <div id="button-yes" className="button" onClick={() => this.toggleWarning()}>I Understand</div>
+          <a id="button-no" className="button" href="https://www.google.com">No Thanks</a>
+        </div>
+      </div> : <Content />
+    )
+  }
+}
+
+export default Warning;
