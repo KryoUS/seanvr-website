c7d2abd9e883b1839f9392fdd94c772a57cd46cd
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-13 06:30:14 -0600

    Added Google Sheets to routes.

--- server/routes/routeIndex.js
+++ server/routes/routeIndex.js
@@ -2,8 +2,10 @@ const routes = require('express').Router();
 
 // Controllers
 const amazon = require('../controllers/amazon');
+const googleSheets = require('../controllers/google-sheets/google-sheets');
 
 //Amazon Gallery
 routes.get('/api/gallery', amazon.getList);
+routes.get('/api/stream/goals', googleSheets.getSheetData);
 
 module.exports = routes;
