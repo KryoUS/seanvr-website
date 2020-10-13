70e0f252eb6c5d4c3c6ae0306c57b8ccd0910390
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-12 22:24:17 -0600

    Fixed Gallery module for Twitch Embeds and Mobile.

--- client/src/components/gallery/gallery.js
+++ client/src/components/gallery/gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import ImageGallery from 'react-image-gallery';
+import Loader from '../loader/loader';
 import './gallery.css';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
@@ -9,25 +10,112 @@ class Gallery extends React.Component {
         super();
 
         this.state = {
-            gallery: []
+            gallery: [],
+            videoSrc: '',
+            showVideo: false,
+            showError: false,
         }
     }
 
     componentDidMount = () => {
         axios.get('/api/gallery').then(res => {
-            this.setState({ gallery: res.data });
+
+            let data = res.data;
+
+            data.push({
+                original: "https://clips-media-assets2.twitch.tv/AT-cm%7C859314223-preview.jpg",
+                thumbnail: "https://clips-media-assets2.twitch.tv/AT-cm%7C859314223-preview.jpg",
+                embedUrl: 'https://clips.twitch.tv/embed?clip=HedonisticTangentialGazelleFrankerZ&parent=localhost',
+                description: "Lone is definitely trying to seduce chat",
+                renderItem: this._renderVideo.bind(this)
+            });
+            this.setState({ gallery: data });
+
         }).catch(err => {
-            console.log('Server API Error!');
+            this.setState({ showError: true });
         });
+    }
+
+    _onSlide(index) {
+        this._resetVideo();
+    }
+
+    _handleInputChange(state, event) {
+        this.setState({ [state]: event.target.value });
+    }
+
+    _handleCheckboxChange(state, event) {
+        this.setState({ [state]: event.target.checked });
+    }
+
+    _handleThumbnailPositionChange(event) {
+        this.setState({ thumbnailPosition: event.target.value });
+    }
+
+    _resetVideo() {
+        this.setState({ showVideo: false, videoSrc: '' });
+    }
+
+    _toggleShowVideo(url) {
+        this.setState(prevState => ({
+            showVideo: !prevState.check, videoSrc: url
+          }));
+    }
+
+    _renderVideo(item) {
+        return (
+            <div>
+                {
+                    this.state.showVideo ?
+                        <div className='video-wrapper'>
+                            <div
+                                className='close-video'
+                                onClick={this._toggleShowVideo.bind(this, item.embedUrl)}
+                            >
+                            </div>
+                            <iframe
+                                width='100%'
+                                height='100%'
+                                src={item.embedUrl}
+                                frameBorder='0'
+                                allowFullScreen
+                                preload="metadata"
+                            >
+                            </iframe>
+                        </div>
+                        :
+                        <div className='video-wrapper' onClick={this._toggleShowVideo.bind(this, item.embedUrl)}>
+                            <div className='play-button'></div>
+                            <img className='image-gallery-image' src={item.original} />
+                            {
+                                item.description &&
+                                <span
+                                    className='image-gallery-description'
+                                    style={{ right: '0', left: 'initial', marginRight: '20px' }}
+                                >
+                                    {item.description}
+                                </span>
+                            }
+                        </div>
+                }
+            </div>
+        );
     }
 
     render() {
         return <div className="flex-row-evenly">
             <div id="section-title">Gallery</div>
             <div id="gallery-container" className="flex-row-center">
-                {this.state.gallery.length > 1 ? 
-                    <ImageGallery items={this.state.gallery} lazyLoad={true} showBullets={true} /> : 
-                    <div>Pictures are loading...</div>}
+                {this.state.gallery.length > 1 ?
+                    <ImageGallery
+                        items={this.state.gallery}
+                        lazyLoad={true}
+                        onSlide={this._onSlide.bind(this)}
+                        showBullets={true}
+                        additionalClass="app-image-gallery"
+                    /> :
+                    <Loader />}
+                {this.state.showError && <div>ERROR MODAL GOES HERE</div>}
             </div>
         </div>
     }
