dc013189932f035abfe8dad01fb24c91271f19ba
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-13 06:29:59 -0600

    Added Google Sheets cron for server.

--- server/cronjobs/cronIndex.js
+++ server/cronjobs/cronIndex.js
@@ -1,9 +1,11 @@
 const CronJob = require('cron').CronJob;
 const amazon = require('../controllers/amazon');
+const googleSheets = require('../controllers/google-sheets/google-sheets');
 
 const minutes = {
     every5: () => new CronJob('00 */5 * * * *', () => {
         amazon.setList();
+        googleSheets.setSheetData();
     }, null, true, 'America/Denver', null, true),
 
 
