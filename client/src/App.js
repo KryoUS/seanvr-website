3079b231420c59ab832ecb0cc4d4d6261a86902d
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-14 04:31:41 -0600

    Moved header to App so it shows on Content Warning Fixed styling issues with this change.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Warning from './components/warning/warning';
+import Socials from './components/socials/socials';
 import './App.css';
-import './Nav.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src="https://d3mb8zw3wuinah.cloudfront.net/seanVRLogo.png" className="App-logo" alt="logo" />
+        <Socials />
+      </header>
       <Warning />
     </div>
   );
