3a69ebd837af8c0c6d8d29a83e8dd37bac88b1a9
Michael Hughes <TaurusSHO89@gmail.com>
2020-10-24 20:55:01 -0600

    Added Error Message handler. Removed testing console logs.

--- client/src/components/stream-goals/stream-goals.js
+++ client/src/components/stream-goals/stream-goals.js
@@ -16,7 +16,6 @@ class StreamGoals extends React.Component {
     componentDidMount = () => {
         axios.get('/api/stream/goals').then(res => {
             this.setState({ sheetData: res.data});
-            console.log(res.data);
         }).catch(err => {
             this.setState({ showError: true });
         });
@@ -30,17 +29,17 @@ class StreamGoals extends React.Component {
         return (
             <div className="stream-goals" ref={this.props.goalsRef}>
                 <div id="stream-goals-title" className="section-title">Stream Goals</div>
-                <div id="stream-goals-subtitle" className="section-title">New Subs, Gifted Subs, and Resubs Count Towards Goal</div>
-                <div id="stream-goals-subtitle" className="section-title">Goals in White Pay Moderators!</div>
+                {!this.state.showError && <div id="stream-goals-subtitle" className="section-title">New Subs, Gifted Subs, and Resubs Count Towards Goal</div>}
+                {!this.state.showError && <div id="stream-goals-subtitle" className="section-title">Goals in White Pay Moderators!</div>}
                 <br />
                 <div id="stream-goals-container" className="flex-row-center">
                     <div className="stream-goals-header flex-row-center">
-                        <div id="stream-goals-cell" className="stream-goals-cell">Subs</div>
-                        <div id="stream-goals-big-cell" className="stream-goals-cell">Description</div>
+                        {!this.state.showError && <div id="stream-goals-cell" className="stream-goals-cell">Subs</div>}
+                        {!this.state.showError && <div id="stream-goals-big-cell" className="stream-goals-cell">Description</div>}
                     </div>
                     {this.state.sheetData.length > 0 ?
                         this.state.sheetData.map(obj => {
-                            return <div style={{ textDecoration: obj.goalReached && 'line-through' }} className="stream-goals-row flex-row-center">
+                            return <div key={'goals'+obj.amount} style={{ textDecoration: obj.goalReached && 'line-through' }} className="stream-goals-row flex-row-center">
                                 <div style={{ 
                                     color: obj.forMods && 'white', 
                                     textShadow: obj.forMods && '2px 2px 10px black' 
@@ -52,8 +51,11 @@ class StreamGoals extends React.Component {
                             </div>
                         })
                         :
-                        <Loader />}
-                    {this.state.showError && <div>ERROR MODAL GOES HERE</div>}
+                        this.state.showError ? 
+                            <div style={{color: 'yellow', fontSize: '1.5rem'}} id="stream-goals-subtitle" className="section-title">There was an error loading the stream goals!<br/>Please try again later...</div> 
+                            : 
+                            <Loader />
+                    }
                 </div>
             </div>
         )
